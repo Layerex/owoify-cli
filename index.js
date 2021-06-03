b6ec3cc3a953dc031f92b9d3edd29f454ae7dca5
Layerex <layerex@dismail.de>
2021-06-03 12:02:04 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const owoify = require("owoify-js").default;
+const readline = require("readline");
+
+const USAGE_STRING = `Usage: owoify [level]
+Levels are:
+- owo (default): pretty vanilla. Not much else to say about it.
+- uwu: moderately infuses the text but it gets wose and wose
+- uvu: litewawwy unweadabwal`;
+
+let level;
+if (process.argv.length > 2) {
+  if (["-h", "--help"].indexOf(process.argv[2]) >= 0) {
+    console.log(USAGE_STRING);
+    process.exit(0);
+  }
+  level = process.argv[2];
+} else {
+  level = "owo";
+}
+
+let rl = readline.createInterface({
+  input: process.stdin,
+});
+
+rl.on("line", (input) => {
+  console.log(owoify(input, level));
+});
+
+rl.on("close", () => {
+  process.exit(0);
+});
